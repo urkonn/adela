616b6bb8d06f4f0417a239b85994c85c96a97ad2
Julio Acuña <urkonn@gmail.com>
2014-02-11 21:09:53 -0600

    testing user authorization

--- adela/urls.py
+++ adela/urls.py
@@ -3,11 +3,12 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-from administradora.views import LandingView
+from administradora.views import LandingView, ProfileView
 
 urlpatterns = patterns('',
                        # Examples:
                        url(r'^admin/', include(admin.site.urls)),
                        url(r'^$', LandingView.as_view(), name='landing'),
-                       url(r'login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'})
+                       url(r'^home/$', ProfileView.as_view(), name='profile'),
+                       url(r'login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
                        )
