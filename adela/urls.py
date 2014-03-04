4e1fd5ad3472738560b15c35148cff385b30c211
Julio Acuña <urkonn@gmail.com>
2014-03-04 09:46:29 -0600

    fake catalog for testing

--- adela/urls.py
+++ adela/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-
+from django.views.generic import TemplateView
 from django.contrib import admin
 admin.autodiscover()
 
@@ -12,4 +12,5 @@ urlpatterns = patterns('',
                        url(r'^home/$', ProfileView.as_view(), name='profile'),
                        url(r'login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
                        url(r'logout/$', 'django.contrib.auth.views.logout', {'template_name': 'landing.html'}),
+                       url(r'^cedn/catalogo\.json$', TemplateView.as_view(template_name='catalogo.json', content_type='aplication/json')),
                        )
