ed00ea5251e1769a97a8de708d4d8799c0ac1aa4
Julio Acuña <urkonn@gmail.com>
2014-03-11 12:42:18 -0600

    work in progress

--- adela/settings.py
+++ adela/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'debug_toolbar',
     'debug_toolbar_extra',
+    'django_extensions',
     'django_pdb',
     'storages',
     'administradora',
--- administradora/models.py
+++ administradora/models.py
@@ -2,7 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Inventory(models.Model):
+class DataAdministrator(models.Model):
     data_admin = models.ForeignKey(User)
+
+
+class Organization(models.Model):
+    data_admin = models.ForeignKey(DataAdministrator)
+    organization_name = models.CharField(max_length=50)
+    organization_shortname = models.CharField(max_length=10)
+
+
+class Inventory(models.Model):
+    organization = models.ForeignKey(Organization)
     data_file = models.FileField(upload_to='inventarios')
     timestamp = models.DateTimeField(auto_now=True)
