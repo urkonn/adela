4c8cdf60fff71c777c6a114bbee30319a22c3dd9
Rod Wilhelmy <rwilhelmy@gmail.com>
2014-03-13 19:29:28 -0600

    Add endpoint for list of catalogs available
    ➜  adela git:(fake-catalog) ✗ curl http://localhost:8000/catalogos.json
    ```json
    [
        "http://adelita.herokuapp.com/cedn/catalogo.json"
    ]
    ```

--- adela/urls.py
+++ adela/urls.py
@@ -13,5 +13,6 @@ urlpatterns = patterns('',
                        url(r'^home/$', 'administradora.views.ProfileView', name='profile'),  #ProfileView.as_view(), name='profile'),
                        url(r'login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
                        url(r'logout/$', 'django.contrib.auth.views.logout', {'template_name': 'landing.html'}),
+                       url(r'^catalogos\.json$', TemplateView.as_view(template_name='catalogos.json', content_type='aplication/json')),
                        url(r'^cedn/catalogo\.json$', TemplateView.as_view(template_name='catalogo.json', content_type='aplication/json')),
                        )
